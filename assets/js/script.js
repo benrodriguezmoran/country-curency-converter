33e8fd008ebffec70ed9e18f4c1a8ca452390947
G303K <micder18@gmail.com>
2023-09-18 20:53:17 -0400

    Updated JS & HTML

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,78 +1,65 @@
-const API_ENDPOINT = 'https://api.freecurrencyapi.com/v1/latest?apikey=fca_live_yK5F5B2HZcfDTAFbadAylTvZSv9Oq2I8qoAFZAqk';
-var currencies = [
-    "EUR",
-    "USD",
-    "JPY",
-    "BGN",
-    "CZK",
-    "DKK",
-    "GBP",
-    "HUF",
-    "PLN",
-    "RON",
-    "SEK",
-    "CHF",
-    "ISK",
-    "NOK",
-    "HRK",
-    "RUB",
-    "TRY",
-    "AUD",
-    "BRL",
-    "CAD",
-    "CNY",
-    "HKD",
-    "IDR",
-    "ILS",
-    "INR",
-    "KRW",
-    "MXN",
-    "MYR",
-    "NZD",
-    "PHP",
-    "SGD",
-    "THB",
-    "ZAR"
-];
+// Function to make an XMLHttpRequest and display information in the flex box
+function fetchDataFromAPI() {
+  var oReq = new XMLHttpRequest();
+  oReq.addEventListener("load", function () {
+    if (oReq.status === 200) {
+      // Parse the JSON response if it's successful
+      try {
+        var response = JSON.parse(this.responseText);
 
- 
-currencies.forEach(currency => {
-    $('.currencyDropdown').forEach(dropdown => {
-        
-    });
-});
-// Function to convert currency
-async function convertCurrency(amount, fromCurrency, toCurrency) {
-    try {
-        const response = await fetch(`${API_ENDPOINT}?apikey=${API_KEY}&from=${fromCurrency}&to=${toCurrency}&amount=${amount}`);
-        if (!response.ok) {
-            throw new Error('Network response was not ok');
-        }
-        
-        const data = await response.json();
-        
-        if (data.result === 'success') {
-            const convertedAmount = data.conversion_result;
-            return convertedAmount;
-        } else {
-            throw new Error('Currency conversion failed');
-        }
-    } catch (error) {
-        console.error('Error:', error);
-        throw error;
+        // Display the information in the flex box
+        displayInformation(response);
+      } catch (error) {
+        console.error("Error parsing JSON:", error);
+      }
+    } else {
+      console.error("Request failed with status:", oReq.status);
     }
+  });
+  oReq.open(
+    "GET",
+    "https://api.freecurrencyapi.com/v1/latest?apikey=fca_live_yK5F5B2HZcfDTAFbadAylTvZSv9Oq2I8qoAFZAqk"
+  );
+  oReq.send();
 }
 
+// Function to display information in the flex box
+function displayInformation(data) {
+  // Select the flex box by its id
+  const infoBox = document.getElementById("info-box");
 
-// Example usage:
-const amount = 100; // Replace with the amount you want to convert
-const fromCurrency = 'USD'; // Replace with the source currency code
-const toCurrency = 'EUR'; // Replace with the target currency code
+  // Clear any previous content in the flex box
+  infoBox.innerHTML = "";
 
-convertCurrency(amount, fromCurrency, toCurrency)
-    .then((result) => {
-        console.log(`Converted amount: ${result}`);
-    })
-    .catch((error) => {
-        console.error('Currency conversion error:', error);
-    });
+  // Create and populate elements with data
+  const infoParagraph = document.createElement("p");
+  infoParagraph.textContent = "Information from the API:";
+
+  // Create a div element to hold the currency data
+  const currencyDataDiv = document.createElement("div");
+  currencyDataDiv.className = "currency-data"; // You can define a CSS class for styling
+
+  // Create and populate elements for the currency data
+  const currencyValue = document.createElement("p");
+  currencyValue.textContent = "Currency Value: " + data.value;
+
+  // Append elements to the currency data div
+  currencyDataDiv.appendChild(currencyValue);
+
+  // Append elements to the flex box
+  infoBox.appendChild(infoParagraph);
+  infoBox.appendChild(currencyDataDiv);
+
+  // Apply styling to the flex box and currency data div
+  infoBox.style.backgroundColor = "lightblue";
+  currencyDataDiv.style.padding = "10px";
+  currencyDataDiv.style.border = "1px solid #ddd";
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+  // Select the "Generate" button by its id
+  const generateButton = document.getElementById("generate-button");
+
+  // Add a click event listener to the button
+  generateButton.addEventListener("click", fetchDataFromAPI);
+});
