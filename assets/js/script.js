3324ac253059c2e98170f7aa15d114f695b23d0d
benrodriguezmoran <ben.rodriguezmoran@gmail.com>
2023-09-24 21:21:02 -0400

    fixed for real

--- assets/js/script.js
+++ assets/js/script.js
@@ -83,5 +83,5 @@ function handleConversion(json) {
   new google.translate.TranslateElement({
     pageLanguage: 'en', // Set the default page language (you can change this)
     includedLanguages: 'af,sq,am,ar,hy,as,ay,az,bm,eu,be,bn,bho,bs,bg,ca,ceb,co,hr,cs,da,dv,doi,nl,en,eo,et,ee,fil,fi,fr,fy,gl,ka,de,el,gn,gu,ht,ha,haw,he,hi,hmn,hu,is,ig,ilo,id,ga,it,ja,jv,kn,kk,km,rw,gom,ko,kri,ku,ckb,ky,lo,la,lv,ln,lt,lg,lb,mk,mai,mg,ms,ml,mt,mi,mr,Mni-Mte,lus,mn,my,ne,no,ny,or,om,ps,fa,pl,pt,pa,qu,ro,ru,sm,sa,gd,nso,sr,st,sn,sd,si,sk,sl,so,es,su,sw,sv,tl,tg,ta,tt,te,th,ti,ts,tr,tk,ak,uk,ur,ug,uz,vi,cy,xh,yi,yo,zu' // Specify the languages you want to support
-  },'google_translate_element');
+  }, 'google_translate_element');
 }
